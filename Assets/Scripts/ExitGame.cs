d698f538ee48c9186884c0b6a5b9049cc0e362ed
Glassnose <rollinsasia@yahoo.com>
2023-05-25 07:50:14 -0700

    Initial commit

---
+++ Assets/Scripts/ExitGame.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void QuitGame ()
+    {
+        Debug.Log("QUIT!");
+
+        Application.Quit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
